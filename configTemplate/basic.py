RealA10N/validit

commit 07d1ea2684531131ebecc92f25f447975baa4d75

⚡ Converted lists to sets where was possible

configTemplate/basic.py
@@ -16,17 +16,16 @@ from .error_managers import (
 class Template:
 
     def __init__(self, *valid_types: typing.Union[type, 'Template']):
-        base_types = list()
-        templates = list()
+        self.base_types, self.templates = set(), set()
 
         for type_ in valid_types:
             if isinstance(type_, type):
                 # If a basic type like `str`, `int`, etc.
-                base_types.append(type_)
+                self.base_types.add(type_)
 
             elif isinstance(type_, Template):
                 # If an instance of a Template
-                templates.append(type_)
+                self.templates.add(type_)
 
             else:
                 raise InvalidTemplateConfiguration(
@@ -34,17 +33,13 @@ class Template:
                     f"instances, not '{type(type_).__name__}'"
                 )
 
-        # Converting valid type lists to tuples
-        self.base_types = tuple(base_types)
-        self.templates = tuple(templates)
-
     def check(self,
               data: typing.Any,
               path: typing.Tuple[str] = tuple(),
               ) -> None:
 
         # Basic type check
-        if isinstance(data, self.base_types):
+        if isinstance(data, tuple(self.base_types)):
             # If one of the accepted basic types, accepts it immediately and
             # doesn't raise an error.
             return

commit a392ea93290fb73fb34be1e53ff776f8a668eb6c

🐛 Fixed bugs caused by 07d1ea2684531131ebecc92f25f447975baa4d75

configTemplate/basic.py
@@ -55,7 +55,7 @@ class Template:
         # If both checks failed, raise an error
         errors.register_error(TemplateCheckInvalidDataError(
             path=path,
-            expected=self.base_types + self.templates,
+            expected=self.base_types | self.templates,
             got=data,
         ))
 

configTemplate/errors.py
@@ -64,7 +64,8 @@ class TemplateCheckInvalidDataError(TemplateCheckError):
         """ A string that represents the expected type """
         formatname = lambda name: f"'{name.__name__}'"
 
-        start, last = self.expected[:-1], self.expected[-1]
+        excepted = list(self.expected)
+        start, last = excepted[:-1], excepted[-1]
         string = ', '.join(formatname(type_) for type_ in start)
 
         if string: