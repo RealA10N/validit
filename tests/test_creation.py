f60aea412753e62c6f68e33dd0e4729e229b38dd
Alon (PC) <downtown2u@gmail.com>
2021-05-26 08:45:09 +0300

    🚚 Changed test order

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -11,15 +11,6 @@ class AnotherObj: pass
 class TestBaseTemplate:
 
     @pytest.mark.parametrize('template', (
-        1, str(), bool(), ExampleObj(), SonOfExample(),
-    ))
-    def test_creation_fails(self, template):
-        """ Test that a template of instance (and not a type) raises an
-        error. """
-        with pytest.raises(InvalidTemplateConfiguration):
-            Template(template)
-
-    @pytest.mark.parametrize('template', (
         (ExampleObj,),
         (int, float, complex),
         (str, bool, AnotherObj),
@@ -29,6 +20,18 @@ class TestBaseTemplate:
         """ Test that a template constractor provided with one or more types
         can be initialized without errors. """
         Template(*template)
+
+    @pytest.mark.parametrize('template', (
+        1, str(), bool(), ExampleObj(), SonOfExample(),
+        (str, int()),
+        (bool, int, float, 'str'),
+        (AnotherObj, ExampleObj(), SonOfExample()),
+    ))
+    def test_creation_fails(self, template):
+        """ Test that a template of instance (and not a type) raises an
+        error. """
+        with pytest.raises(InvalidTemplateConfiguration):
+            Template(template)
 
 
 class TestTemplateDict:
