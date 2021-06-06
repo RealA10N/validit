48b546a3bf929db2f08eb2910a9f498ab497279c
Alon (PC) <downtown2u@gmail.com>
2021-06-06 23:59:46 +0300

    ⚡ Added `all` dunder

--- configTemplate/__init__.py
+++ configTemplate/__init__.py
@@ -1,2 +1,5 @@
 from .templates import Template, TemplateDict, TemplateList, TemplateOptional
 from .files import JsonFileLoader, YamlFileLoader
+
+__all__ = ['Template', 'TemplateDict', 'TemplateList', 'TemplateOptional',
+           'JsonFileLoader', 'YamlFileLoader']
