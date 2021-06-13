79ac0e0796e8595d49210d6ea7421c65fdf7649a
Alon (PC) <downtown2u@gmail.com>
2021-06-13 20:17:02 +0300

    ✅ Added `container_dump` tests for `Optional` object

--- tests/test_container_dump.py
+++ tests/test_container_dump.py
@@ -6,6 +6,7 @@ from configTemplate.containers import HeadContainer
 from configTemplate import (
     Template,
     TemplateDict,
+    Optional,
 )
 
 cases = [
@@ -50,6 +51,36 @@ cases = [
                 'out': {'user': 'A10N', 'code': 123}},
         ),
     },
+    {
+        'name': 'optional-no-default',
+        'template': Optional(Template(str)),
+        'cases': (
+            {'in': None, 'out': None},
+            {'in': 'string', 'out': 'string'},
+            {'in': DefaultValue, 'out': DefaultValue},
+        ),
+    },
+    {
+        'name': 'optional-with-default',
+        'template': Optional(Template(str), default='DEFAULT'),
+        'cases': (
+            {'in': None, 'out': None},
+            {'in': 'string', 'out': 'string'},
+            {'in': DefaultValue, 'out': 'DEFAULT'},
+        ),
+    },
+    {
+        'name': 'optional-dict',
+        'template': TemplateDict(name=Optional(Template(str), default='Unknown')),
+        'cases': (
+            {'in': {}, 'out': {'name': 'Unknown'}},
+            {'in': {'age': 17}, 'out': {'name': 'Unknown'}},
+            {'in': {'name': 'Alon'}, 'out': {'name': 'Alon'}},
+            {'in': {'name': 'Alon', 'age': 17}, 'out': {'name': 'Alon'}},
+            {'in': None, 'out': {'name': 'Unknown'}},
+            {'in': DefaultValue, 'out': {'name': 'Unknown'}},
+        ),
+    }
 ]
 
 
