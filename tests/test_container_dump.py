8bb26412d35c64a9277a2a2c50a14dde23bb256b
Alon (PC) <downtown2u@gmail.com>
2021-06-13 19:59:18 +0300

    ✅ Added `container_dump` tests for `TemplateDict`

--- tests/test_container_dump.py
+++ tests/test_container_dump.py
@@ -1,16 +1,53 @@
 import pytest
-from configTemplate import Template
+
 from configTemplate.utils import DefaultValue
 from configTemplate.containers import HeadContainer
 
+from configTemplate import (
+    Template,
+    TemplateDict,
+)
+
 cases = [
     {
-        'name': 'base-template',
+        'name': 'string',
         'template': Template(str),
         'cases': (
             {'in': 'Hello', 'out': 'Hello'},
             {'in': str(), 'out': ''},
             {'in': DefaultValue, 'out': DefaultValue},
+        ),
+    },
+    {
+        'name': 'dict',
+        'template': TemplateDict(user=Template(str), code=Template(int)),
+        'cases': (
+            {'in': None,
+                'out': {}},
+            {'in': 123,
+                'out': {}},
+            {'in': {},
+                'out': {}},
+            {'in': {'user': 123},
+                'out': {'user': 123}},
+            {'in': {'user': None},
+                'out': {'user': None}},
+            {'in': {'user': DefaultValue},
+                'out': {}},
+            {'in': {'user': 'A10N'},
+                'out': {'user': 'A10N'}},
+            {'in': {'other': None},
+                'out': {}},
+            {'in': {'other': 'A10N'},
+                'out': {}},
+            {'in': {'user': 'A10N', 'other': 'A10N'},
+                'out': {'user': 'A10N'}},
+            {'in': {'user': 123, 'code': 'A10N'},
+                'out': {'user': 123, 'code': 'A10N'}},
+            {'in': {'user': 'A10N', 'code': 123},
+                'out': {'user': 'A10N', 'code': 123}},
+            {'in': {'user': 'A10N', 'code': 123, 'other': None},
+                'out': {'user': 'A10N', 'code': 123}},
         ),
     },
 ]
@@ -25,7 +62,7 @@ def generate_params():
                 test['template'],
                 case['in'],
                 case['out'],
-                id=test['name'],
+                id=test.get('name'),
             ))
 
     return params
