f3f970d276637b725614d946e3e4cc088e739f07
Alon (Ubuntu) <downtown2u@gmail.com>
2022-01-04 13:25:28 +0200

    ✨ Added `__repr__` to Schema

--- validit/schema.py
+++ validit/schema.py
@@ -9,3 +9,9 @@ class Schema(ABC):
         the current schema. This returns a generator that will yield
         'ValidationError' instances for every error in the data.
         The generator won't yield any values if the data is valid. """
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        """ An abstract method that should return a string that represents the
+        schema. This string may be used is validation error messages, if
+        validation fails. """
--- validit/string.py
+++ validit/string.py
@@ -15,3 +15,9 @@ class String(Schema):
         elif self.pattern is not None:
             if not self.pattern.fullmatch(data):
                 yield ValidationRegexError(data, self.pattern)
+
+    def __repr__(self) -> str:
+        extra = str()
+        if self.pattern is not None:
+            extra = f'[{self.pattern.pattern}]'
+        return 'String' + extra
