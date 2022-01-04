60edc73fdcdb68deeec38bbf7c6462f5e0895507
Alon (Ubuntu) <downtown2u@gmail.com>
2022-01-04 14:08:48 +0200

    ✨ Added `Boolean` Schema

--- validit/__init__.py
+++ validit/__init__.py
@@ -1,7 +1,8 @@
 from .schema import Schema
 from .string import String
+from .boolean import Boolean
 
-__all__ = ['Schema', 'String']
+__all__ = ['Schema', 'String', 'Boolean']
 
 __version__ = '2.0.0-dev'
 __author__ = 'Alon Krymgand Osovsky'
